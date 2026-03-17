449b4c90d6fa91e9df93b0819b7c17d2bc03f186
Aroa Cotrufo <aroa.c97@gmail.com>
2026-03-17 18:43:52 +0100

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,55 @@ import { useState } from 'react'
 
 
 
- const films = [
-        { title: 'Inception', genre: 'Fantascienza' },
-        { title: 'Il Padrino', genre: 'Thriller' },
-        { title: 'Titanic', genre: 'Romantico' },
-        { title: 'Batman', genre: 'Azione' },
-        { title: 'Interstellar', genre: 'Fantascienza' },
-        { title: 'Pulp Fiction', genre: 'Thriller' },
-    ]
-    function App()
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+function App() {
+
+  const [genre, setGenre] = useState("")
+  const [filter, setFilter] = useState([])
+
+
+  useEffect(() => {
+
+    if (genre === '') {
+      setFilter(films)
+    } else {
+      setFilter(films.filter(films => films.genre === genre))
+    }
+
+  }, [genre])
+  return (
+    <div className='container py-5'>
+      <h1 className="text-center display-4 mb-5">Lista Film</h1>
+      <div className='row justify-content-center-mb-5'>
+        <label className='form-label fw-bold fs-5 d-block mb-2'>Filtra per Genere:</label>
+        <select className='form-select form-select-lg'
+          value={genre}
+          onChange={e => setGenre(e.target.value)}>
+          <option value="">Tutti</option>
+          <option value="Fantascienza">Fantascienza</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Romantico">Romantico</option>
+          <option value="Azione">Azione</option>
+        </select>
+      </div>
+    </div>
+  
+    <div className = 'row g-4' >
     
-    const [genre, setGenre] = useState("")
-    const [filter, setFilter] = useState([])
+      {filter.map((f, i) => (
+        <div key={i}
+          className='col-lg-6 col-xl-4'>
+          <div className='card -body p-4'>
+            <h3 className='card-title'>{f.title}</h3>
+          </div>
+        </div>
+      </div>
+      ))}
 
-
-    useEffect(() => {
-      
-            if(genre=== ''){
-              setFilter(films)
-            }else{
-              setFilter(films.filter(films => films.genre=== genre))
-            }
-
-    },[genre])
-    return(
-      
-    )
-
-export default App
