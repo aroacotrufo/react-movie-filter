968bc1537de9e3030615c3647864d7acb7cd2651
Aroa Cotrufo <aroa.c97@gmail.com>
2026-03-17 17:57:56 +0100

    modifiche

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 
 
 
-function App() {
-  const [count, setCount] = useState(0)
+ const films = [
+        { title: 'Inception', genre: 'Fantascienza' },
+        { title: 'Il Padrino', genre: 'Thriller' },
+        { title: 'Titanic', genre: 'Romantico' },
+        { title: 'Batman', genre: 'Azione' },
+        { title: 'Interstellar', genre: 'Fantascienza' },
+        { title: 'Pulp Fiction', genre: 'Thriller' },
+    ]
+    function App()
+    
+    const [genre, setGenre] = useState("")
+    const [filter, setFilter] = useState([])
 
-  return (
-    <>
-     
-    </>
-  )
-}
+
+    useEffect(() => {
+      
+            if(genre=== ''){
+              setFilter(films)
+            }else{
+              setFilter(films.filter(films => films.genre=== genre))
+            }
+
+    },[genre])
+    return(
+      
+    )
 
 export default App
